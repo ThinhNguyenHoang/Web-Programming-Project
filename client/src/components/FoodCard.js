0d97a58d24e6204fda36a005ca9274ff341c03aa
KHOI <t.hoangkhoi312@gmail.com>
2021-10-21 09:10:39 +0700

    ================Khôi============= Detail: +Sửa lại component foodcard

--- client/src/components/FoodCard.js
+++ client/src/components/FoodCard.js
@@ -6,26 +6,34 @@ import CardContent from '@mui/material/CardContent';
 import CardActions from '@mui/material/CardActions';
 import fimg from "../assets/images/foodimg.png"
 
+const initfood={
+    fimg:fimg,
+    foodname:"Food Name",
+    foodtype:"Category",
+    fooddescrip:"Fry your onion, peppers and garlic in olive oil until nicely translucent.\nMake a well in your veg and add your chicken.Add your seasoning and salt.",
+    price:"300K"
+}
 
-function FoodCard ({foodimg=fimg,foodname="Food Name",foodtype="Category",fooddescrip="Fry your onion, peppers and garlic in olive oil until nicely translucent.\nMake a well in your veg and add your chicken.Add your seasoning and salt.",price="300K"}) {
+
+function FoodCard ({foodinfo=initfood}) {
     return (
         <Card sx={{m:1,maxWidth:300}}>
             <CardMedia
                 component="img"
                 height="194"
-                image={foodimg}
-                alt={foodname}/>
+                image={foodinfo.foodimg}
+                alt={foodinfo.foodname}/>
             <CardHeader
-              subheader={foodtype}
-              title={foodname}
+              subheader={foodinfo.foodtype}
+              title={foodinfo.foodname}
             />
             <CardContent>
-            <Typography variant="body2" color="text.secondary">{fooddescrip}</Typography>
+            <Typography variant="body2" color="text.secondary">{foodinfo.fooddescrip}</Typography>
             </CardContent>
             <CardActions>
                 <Button size="small">Order</Button>
                 <Button size="small">Wishlist</Button>
-                <Button size="small">{price}</Button>
+                <Button size="small">{foodinfo.price}</Button>
             </CardActions>
             
 
