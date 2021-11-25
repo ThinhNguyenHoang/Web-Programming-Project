6f4db91239deea5de9dc12779b77a10b8205fbab
Thinh Nguyen Hoang <thinh.nguyenbk2018@hcmut.edu.vn>
2021-11-26 03:33:28 +0700

    update bank_account with popover

--- client/src/components/Payment/PaymentDrawer.js
+++ client/src/components/Payment/PaymentDrawer.js
@@ -1,13 +1,13 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Button, Drawer, ImageList, ImageListItem, Popover} from "@mui/material";
+import React, { useEffect, useState } from 'react';
+import { Box, Button, Drawer, ImageList, ImageListItem, Popover } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import {useDispatch, useSelector} from "react-redux";
-import {selectors} from "../../redux/slices/auth/AuthSlice";
-import {getDownloadURL, listAll, ref} from "firebase/storage";
-import {ImageUploader, storage} from "../../utils/UploadFile/FileUploader";
+import { useDispatch, useSelector } from "react-redux";
+import { selectors } from "../../redux/slices/auth/AuthSlice";
+import { getDownloadURL, listAll, ref } from "firebase/storage";
+import { ImageUploader, storage } from "../../utils/UploadFile/FileUploader";
 import Typography from "@mui/material/Typography";
-import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBackspace, faPlus} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBackspace, faPlus } from "@fortawesome/free-solid-svg-icons";
 import Toaster from "../../utils/Toaster/Toaster";
 import default_account_image from '../../assets/images/default_bank_account_img.png'
 import CardMedia from "@mui/material/CardMedia";
@@ -48,9 +48,9 @@ export const bank_account_init_list = [{
     valid_end: "",
 }]
 
-export const BankAccountItem = ({account_item, additionalStyle}) => {
+export const BankAccountItem = ({ account_item, additionalStyle }) => {
     return (
-        <Card sx={{maxWidth: 345, ...additionalStyle}}>
+        <Card sx={{ maxWidth: 345, ...additionalStyle }}>
             <CardMedia
                 component="img"
                 height="140"
@@ -75,29 +75,29 @@ export const BankAccountItem = ({account_item, additionalStyle}) => {
     );
 }
 
-export const AccountGrid = ({bank_account_list, callback}) => {
+export const AccountGrid = ({ bank_account_list, callback }) => {
     const [chosenAccount, setChosenAccount] = useState(0);
     return (
-        <Box sx={{flexGrow: 1, m: 4}}>
+        <Box sx={{ flexGrow: 1, m: 4 }}>
             {
                 bank_account_list ?
                     <Grid
                         container
-                        spacing={{xs: 2, md: 2}}
-                        columns={{xs: 4, sm: 8, md: 12}}
+                        spacing={{ xs: 2, md: 2 }}
+                        columns={{ xs: 4, sm: 8, md: 12 }}
                     >
                         {bank_account_list.map((item, index) => (
                             <Grid item
-                                  xs={2} sm={4} md={4} key={index} onClick={() => {
-                                setChosenAccount(index);
-                                if (callback) {
-                                    callback(chosenAccount);
-                                }
-                            }}>
+                                xs={2} sm={4} md={4} key={index} onClick={() => {
+                                    setChosenAccount(index);
+                                    if (callback) {
+                                        callback(chosenAccount);
+                                    }
+                                }}>
                                 <BankAccountItem account_item={item} additionalStyle={chosenAccount === index ? {
                                     border: 4,
                                     borderColor: `primary.main`
-                                } : {}}/>
+                                } : {}} />
                             </Grid>
                         ))}
                     </Grid>
@@ -114,66 +114,93 @@ export const PaymentDrawer = (
     {
         trigger, additionalStyle = {}, amount_to_pay = 0
     }
-    ) => {
-        const dispatch = useDispatch();
-        const [show, setShow] = useState(false);
-        const [indexChosen, setIndexChosen] = useState(0);
-        // TODO GET THE REAL BANK ACCOUNT LIST AND ADD IT TO THE GRID
-        // const [accountList,setAccountList]  = useState([]);
-        return (
-            <Box sx={{m: 1, p: 1, display: `flex`, flexDirection: `column`}} xs={12} md={4}
-                 justifyContent={`flex-start`} alignItems={`center`}>
-                <Box sx={{
-                    py: 3,
-                    color: `black`,
-                    display: `flex`,
-                    flexDirection: `column`,
-                    alignItems: `center`,
-                    justifyContent: `center`
+) => {
+    const dispatch = useDispatch();
+    const [show, setShow] = useState(false);
+    const [indexChosen, setIndexChosen] = useState(0);
+
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const showPopOver = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const closePopOver = () => {
+        setAnchorEl(null);
+    };
+    // TODO GET THE REAL BANK ACCOUNT LIST AND ADD IT TO THE GRID
+    // const [accountList,setAccountList]  = useState([]);
+    return (
+        <Box sx={{ m: 1, p: 1, display: `flex`, flexDirection: `column` }} xs={12} md={4}
+            justifyContent={`flex-start`} alignItems={`center`}>
+            <Box sx={{
+                py: 3,
+                color: `black`,
+                display: `flex`,
+                flexDirection: `column`,
+                alignItems: `center`,
+                justifyContent: `center`
+            }}>
+                <Box sx={{ ...additionalStyle }} onClick={() => {
+                    setShow(true)
                 }}>
-                    <Box sx={{...additionalStyle}} onClick={() => {
-                        setShow(true)
-                    }}>
-                        {/*<img src={image_uri ? image_uri :default_user_avatar} alt={`image`}/>*/}
-                        {trigger}
-                    </Box>
-                    <Drawer sx={{display: `flex`, flexDirection: `column`, alignItems: `center`}} open={show}
-                            anchor={`bottom`} onClose={() => setShow(false)}>
-                        <Box sx={{
-                            display: `flex`,
-                            flexDirection: `row`,
-                            alignItems: `center`,
-                            justifyContent: `space-between`
-                        }}>
-                            <Typography variant={`h6`} sx={{m: 3}}>
-                                {"Your Payment Accounts"}
-                            </Typography>
-                            <Box>
-                                <Button sx={{mx: 2}} variant={`contained`} color={`primary`}>
-                                    <Box sx={{mr: 1}}>
-                                        <FontAwesomeIcon style={{color: '#fff'}} icon={faPlus}/>
-                                    </Box>
-                                    Add Payment Account
-                                </Button>
-                                <Button sx={{mx: 2}} variant={`contained`} color={`primary`} onClick={() => setShow(false)}>
-                                    <Box sx={{mr: 1}}>
-                                        <FontAwesomeIcon style={{color: '#fff'}} icon={faBackspace}/>
-                                    </Box>
-                                    CLOSE
-                                </Button>
-                            </Box>
-                        </Box>
-                        <AccountGrid bank_account_list={bank_account_init_list}
-                                     callback={(index) => setIndexChosen(index)}/>
-                        {amount_to_pay && <Typography sx={{alignSelf: `center`, m: 2}} variant={`h3`}
-                                     color={`${bank_account_init_list[indexChosen].balance >= amount_to_pay ? 'success.main' : 'error.main'}`}>
-                            {bank_account_init_list[indexChosen].balance < amount_to_pay ? "Insufficent Balance To Pay" : "Valid account with enough money"}
-                        </Typography>}
-                        {/*<MyImageMasonry/>*/}
-                    </Drawer>
+                    {/*<img src={image_uri ? image_uri :default_user_avatar} alt={`image`}/>*/}
+                    {trigger}
                 </Box>
+                <Drawer sx={{ display: `flex`, flexDirection: `column`, alignItems: `center` }} open={show}
+                    anchor={`bottom`} onClose={() => setShow(false)}>
+                    <Box sx={{
+                        display: `flex`,
+                        flexDirection: `row`,
+                        alignItems: `center`,
+                        justifyContent: `space-between`
+                    }}>
+                        <Typography variant={`h6`} sx={{ m: 3 }}>
+                            {"Your Payment Accounts"}
+                        </Typography>
+                        <Box>
+                            <Button sx={{ mx: 2 }} variant={`contained`} color={`primary`}>
+                                <Box sx={{ mr: 1 }}>
+                                    <FontAwesomeIcon style={{ color: '#fff' }} icon={faPlus} />
+                                </Box>
+                                Add Payment Account
+                            </Button>
+
+                            <Button sx={{ mx: 2 }} variant={`contained`} color={`primary`} onClick={() => setShow(false)}>
+                                <Box sx={{ mr: 1 }}>
+                                    <FontAwesomeIcon style={{ color: '#fff' }} icon={faBackspace} />
+                                </Box>
+                                CLOSE
+                            </Button>
+                            {/* Popover to add new bank account */}
+                            <Popover
+                                id={id}
+                                open={open}
+                                anchorEl={anchorEl}
+                                onClose={closePopOver}
+                                anchorOrigin={{
+                                    vertical: 'bottom',
+                                    horizontal: 'left',
+                                }}
+                            >
+                                {/* Form to add bank account */}
+
+                            </Popover>
+
+                        </Box>
+                    </Box>
+                    <AccountGrid bank_account_list={bank_account_init_list}
+                        callback={(index) => setIndexChosen(index)} />
+                    {amount_to_pay && <Typography sx={{ alignSelf: `center`, m: 2 }} variant={`h3`}
+                        color={`${bank_account_init_list[indexChosen].balance >= amount_to_pay ? 'success.main' : 'error.main'}`}>
+                        {bank_account_init_list[indexChosen].balance < amount_to_pay ? "Insufficent Balance To Pay" : "Valid account with enough money"}
+                    </Typography>}
+                    {/*<MyImageMasonry/>*/}
+                </Drawer>
             </Box>
-        );
-    }
-;
+        </Box>
+    );
+}
+    ;
 export default PaymentDrawer;
