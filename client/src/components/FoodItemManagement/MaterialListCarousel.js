e99d759932f2c652a6065fa280aa0ef1e4ce71aa
DESKTOP-TE85INC\admin <none>
2021-11-29 01:06:15 +0700

    ============ Thinh =========== Fix carousel not showing

--- client/src/components/FoodItemManagement/MaterialListCarousel.js
+++ client/src/components/FoodItemManagement/MaterialListCarousel.js
@@ -4,69 +4,19 @@ import Box from "@mui/material/Box";
 import Carousel from "react-material-ui-carousel";
 import Card from "@mui/material/Card";
 import CardMedia from "@mui/material/CardMedia";
-import CardHeader from "@mui/material/CardHeader";
 import CardContent from "@mui/material/CardContent";
-import CardActions from "@mui/material/CardActions";
-import {ThemedOutlineButton} from "../Buttons/ThemedButton/ThemedButton";
-import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
-import Collapse from "@mui/material/Collapse";
+
 import {array_to_chunks} from "../../utils";
 import Typography from '@mui/material/Typography';
 
-const involved_company_init = [
-    {
-        id: "0",
-        logo: "https://www.logodesign.net/logo/building-on-crescent-4303ld.png",
-        name: 'Company 1'
-    },
-    {
-        id: "1",
-        logo: "https://ephoto360.com/uploads/worigin/2019/10/04/Tao_logo_cong_ty_-_Logo_doanh_nghiep_truc_tuyen_tot_nhat5d96f2ae01939_c43f7da2af7c09b3d6d8836ae5f9f02d.jpg",
-        name: 'Company 2'
-    },
-    {
-        id: "3",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTXKbSB2O4yiwr4UgZ_5ZQrOKsWjcRdrZr5Mw&usqp=CAU",
-        name: 'Company 4'
-    },
-    {
-        id: "4",
-        logo: "https://www.logodesign.net/logo/building-on-crescent-4303ld.png",
-        name: 'Company 1'
-    },
-    {
-        id: "5",
-        logo: "https://ephoto360.com/uploads/worigin/2019/10/04/Tao_logo_cong_ty_-_Logo_doanh_nghiep_truc_tuyen_tot_nhat5d96f2ae01939_c43f7da2af7c09b3d6d8836ae5f9f02d.jpg",
-        name: 'Company 2'
-    },
-    {
-        id: "6",
-        logo: "https://gigamall.com.vn/data/2019/09/20/14514819_LOGO-THE-PIZZA-COMPANY-500x500.jpg",
-        name: 'Com 123'
-    },
-    {
-        id: "7",
-        logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT3prvkJeIxpLgp950D476MQV5reNYUkM_ZZN5iy6CHdWIBjLqaPSA33wwhJ0GXSWxpDho&usqp=CAU",
-        name: 'Company 1'
-    },
-    {
-        id: "8",
-        logo: "https://thumbs.dreamstime.com/z/food-logo-smile-label-company-grocery-store-friendly-vector-illustration-smiling-mouth-symbol-135565322.jpg",
-        name: 'Company 2'
-    },
-    {
-        id: "9",
-        logo: "https://i.pinimg.com/600x315/fc/59/3b/fc593b7131868f87ac0a52fa1a496e1d.jpg",
-        name: 'Company 4'
-    },
-]
+
 export const CompanyItem = ({company, mx,sx},...props) => {
     return (
         <Card sx={{ width: 130, maxWidth:200 , minWidth: 200}}>
           <CardMedia
             component="img"
             height="140px"
-            image={company.Picture ? company.logo : default_company_logo}
+            image={company.Picture ? company.Picture : default_company_logo}
             alt={company.MaterialName ? company.MaterialName : "No info"}
           />
           <CardContent sx={{bgcolor:'elevation.layer1.main', color: 'elevation.layer1.contrast'}}>
