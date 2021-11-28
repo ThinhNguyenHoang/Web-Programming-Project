e04c4ce500e6fa32905444783ea425dd0c656576
phuocthuan14 <phuocthuan@vn-cubesystem.com>
2021-11-29 01:14:47 +0700

    THUAN update endpoint transaction

--- server/src/transaction/entity/Transaction.php
+++ server/src/transaction/entity/Transaction.php
@@ -8,19 +8,19 @@ class Transaction
     public $time;
     public $description;
     public $amount;
-    public $order_id;
+    public $voucher_id;
     public $user_id;
 
     /**
      * Transaction constructor.
      */
-    public function __construct($bank_account_number="", $description="", $amount=0, $order_id=0, $user_id=0)
+    public function __construct($bank_account_number="", $description="", $amount=0, $voucher_id=0, $user_id=0)
     {
         date_default_timezone_set('Asia/Saigon');
         $this->time= date("Y/m/d H:i:s");
         $this->description = $description;
         $this->amount = $amount;
-        $this->order_id = $order_id;
+        $this->voucher_id = $voucher_id;
         $this->user_id = $user_id;
         $this->bank_account_number = $bank_account_number;
     }
