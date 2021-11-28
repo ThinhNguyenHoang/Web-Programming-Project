ae419c21fd09ffd8094e5dab9c3b179f7c7de005
DESKTOP-TE85INC\admin <none>
2021-11-29 00:43:34 +0700

    ================= Thinh ================ Detail : + Thêm hình cho material và food + Chỉnh sửa lại Recommendation

--- Database Queries/CREATE_QUERY.sql
+++ Database Queries/CREATE_QUERY.sql
@@ -62,10 +62,11 @@ CREATE TABLE BANK_ACCOUNT(
     valid_end DATE,
     PRIMARY KEY (Id)
 ); 
-INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(1,'900119775222544','Nguyen Hoang THinh','OCB','150000','2000-01-01','2025-10-01');
-INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(2,'900119771114','Thinh Nguyen Hoang','Vietcombank','2250000','2000-01-01','2025-10-01');
-INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(3,'900148974454','Hoang THinh Nguyen','BIDV','150000','2000-01-01','2025-10-01');
-INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(4,'80119775222544','Thinh Thinh Thinh','Algribank','1150000','2000-01-01','2025-10-01');
+-- INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(1,'900119775222544','Nguyen Hoang THinh','OCB','150000','2000-01-01','2025-10-01');
+-- INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(2,'900119771114','Thinh Nguyen Hoang','Vietcombank','2250000','2000-01-01','2025-10-01');
+-- INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(3,'900148974454','Hoang THinh Nguyen','BIDV','150000','2000-01-01','2025-10-01');
+-- INSERT INTO BANK_ACCOUNT(user_id,bank_account_number, bank_account_owner, bank_account_type, balance, valid_start, valid_end) VALUES(4,'80119775222544','Thinh Thinh Thinh','Algribank','1150000','2000-01-01','2025-10-01');
+INSERT INTO `bank_account` VALUES (1,1,'900119775222544','Nguyen Hoang THinh','OCB',150000,'2000-01-01','2025-10-01'),(2,2,'900119771114','Thinh Nguyen Hoang','Vietcombank',2250000,'2000-01-01','2025-10-01'),(3,3,'900148974454','Hoang THinh Nguyen','BIDV',150000,'2000-01-01','2025-10-01'),(4,4,'80119775222544','Thinh Thinh Thinh','Algribank',1150000,'2000-01-01','2025-10-01');
 
 /*
 DROP TABLE IF EXISTS cart; 
@@ -101,24 +102,32 @@ INSERT INTO `web_food`.`cart` (`FoodID`, `ComboID`, `UserID`, `Quantity`) VALUES
 INSERT INTO `web_food`.`cart` (`FoodID`, `ComboID`, `UserID`, `Quantity`) VALUES ( '0', 4, '2','1');
 INSERT INTO `web_food`.`cart` (`FoodID`, `ComboID`, `UserID`, `Quantity`) VALUES ( '0', 5, '4','1');
 
+DROP TABLE IF EXISTS `food`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `food` (
+  `FoodID` bigint NOT NULL AUTO_INCREMENT,
+  `FoodName` varchar(255) NOT NULL,
+  `Picture` text,
+  `Price` int NOT NULL,
+  `Description` text,
+  `Instruct` text,
+  `Sale` bigint DEFAULT NULL,
+  PRIMARY KEY (`FoodID`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `food`
+--
+
+LOCK TABLES `food` WRITE;
+/*!40000 ALTER TABLE `food` DISABLE KEYS */;
+INSERT INTO `food` VALUES (1,'Thịt lợn rim tiêu','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2F7ttta31593350374.jpg?alt=media&token=3273e990-5d69-4773-a9cf-9dd71c5fad09',120000,'Thịt kho là món mặn dùng chính trong bữa cơm của người Việt, bên cạnh món canh và món rau. Hôm nay bếp kho quẹt sẽ giới thiệu đến các bạn món thịt kho tiêu, cũng là món được các chị em thường xuyên chế biến, nhưng để làm đúng chuẩn về màu sắc và mùi vị thì chắc hẳn nhiều người còn bỏ ngỏ. Nào, còn chờ gì nữa, chúng ta bắt tay vào làm món thịt kho tiêu cho kịp bữa cơm chiều nhé!','',0),(2,'Trứng đúc thịt','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2Frc2.jpg?alt=media&token=344ef6ab-53ce-4ca4-93f5-1d7245f17f14',80000,'Món trứng đúc thịt là một món dễ làm, dễ ăn. Đặc biệt là trẻ em, gần như bé nào cũng thích ăn trứng nhưng đôi khi lại không thích ăn thịt vì dai. Với cách làm trứng đúc thịt này, thịt mềm và ngon, trứng rán vàng thơm lừng khắp cả nhà, hương vị rất kích thích với các bé. Hơn nữa vào những ngày bận rộn Bạn chỉ cần mất vài phút là đã có ngay một món ăn thơm ngon mà vẫn đầy đủ chất dinh dưỡng cho cả nhà rồi.','',0),(3,'Đậu sốt cà chua','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2F1529442316.jpg?alt=media&token=c95c7a15-e7f6-49ca-a388-e7aac79182a9',60000,'Đậu sốt cà chua đúng như tên gọi, đây là món ăn với thành phần chính với đậu phụ được rán giòn sau đó đem sốt với cà chua. Bằng việc đem sốt với cà chua, từng miếng đậu được khoác thêm lớp áo đỏ đẹp mắt với hương vị đậm đà hơn.','',0),(4,'Canh chua thịt','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2Fmit-thai-cai-be-2-800x800.jpg?alt=media&token=f521c714-d881-4d5a-9dc5-b0f5065136e0',60000,'Món canh cà chua thịt bằm là một trong những món ăn phổ biến và dễ làm nhất trong các bữa ăn hàng ngày. Món ăn này sẽ mang lại hương vị thơm ngon, ăn mãi không chán bất kể trời nóng hay lạnh cho người thân của bạn. Bạn có thể ăn kèm món canh này cùng với món tôm chiên giòn cho bữa ăn thêm tròn vị.','',0),(5,'Bò nướng lá lốt','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2Fxrptpq1483909204.jpg?alt=media&token=c17810b8-5ffe-4d95-bcb5-d8b335421240',270000,'Bò nướng lá lốt hay còn gọi là bò lá lốt hoặc là thịt bò lá lốt hay bò cuốn lá lốt là một món ăn trong ẩm thực Việt Nam, thịnh hành ở vùng Nam bộ, nguyên liệu chính là thịt bò và lá lốt được chế biến theo phương pháp nướng, có thể kèm theo mỡ chài. Các loại rau sống ăn kèm rất phong phú như: xà lách, húng quế, diếp cá, chuối chát, dưa leo, khế và chấm mắm nêm. Món này đặc trưng bởi vị hấp dẫn với vị ngon của thịt bò nướng lá lốt, béo của đậu phộng hòa chung vị chát của chuối, chua của khế, vị thanh thanh của nhiều loại rau giòn mát, cùng hương mắm nêm cay cay','',0);
+/*!40000 ALTER TABLE `food` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
-DROP TABLE IF EXISTS food; 
-create table Food(
-    FoodID bigint(8) NOT NULL AUTO_INCREMENT,
-    FoodName varchar(255) NOT NULL,
-    Picture text ,
-    Price int NOT NULL,
-    Description text,
-    Instruct text,
-    Sale bigint(8),
-    primary key (FoodID)
-);
-INSERT INTO `web_food`.`food` (`FoodName`,`Picture`,`Price`,`Description`, `Instruct`, `Sale`) VALUES ('Thịt lợn rim tiêu',NULL,120000,'Thịt kho là món mặn dùng chính trong bữa cơm của người Việt, bên cạnh món canh và món rau. Hôm nay bếp kho quẹt sẽ giới thiệu đến các bạn món thịt kho tiêu, cũng là món được các chị em thường xuyên chế biến, nhưng để làm đúng chuẩn về màu sắc và mùi vị thì chắc hẳn nhiều người còn bỏ ngỏ. Nào, còn chờ gì nữa, chúng ta bắt tay vào làm món thịt kho tiêu cho kịp bữa cơm chiều nhé!', '', 0);
-INSERT INTO `web_food`.`food` (`FoodName`,`Picture`,`Price`,`Description`, `Instruct`, `Sale`) VALUES ('Trứng đúc thịt',NULL,80000,'Món trứng đúc thịt là một món dễ làm, dễ ăn. Đặc biệt là trẻ em, gần như bé nào cũng thích ăn trứng nhưng đôi khi lại không thích ăn thịt vì dai. Với cách làm trứng đúc thịt này, thịt mềm và ngon, trứng rán vàng thơm lừng khắp cả nhà, hương vị rất kích thích với các bé. Hơn nữa vào những ngày bận rộn Bạn chỉ cần mất vài phút là đã có ngay một món ăn thơm ngon mà vẫn đầy đủ chất dinh dưỡng cho cả nhà rồi.', '', 0);
-INSERT INTO `web_food`.`food` (`FoodName`,`Picture`,`Price`,`Description`, `Instruct`, `Sale`) VALUES ('Đậu sốt cà chua',NULL,60000,'Đậu sốt cà chua đúng như tên gọi, đây là món ăn với thành phần chính với đậu phụ được rán giòn sau đó đem sốt với cà chua. Bằng việc đem sốt với cà chua, từng miếng đậu được khoác thêm lớp áo đỏ đẹp mắt với hương vị đậm đà hơn.', '', 0);
-INSERT INTO `web_food`.`food` (`FoodName`,`Picture`,`Price`,`Description`, `Instruct`, `Sale`) VALUES ('Canh chua thịt',NULL,60000,'Món canh cà chua thịt bằm là một trong những món ăn phổ biến và dễ làm nhất trong các bữa ăn hàng ngày. Món ăn này sẽ mang lại hương vị thơm ngon, ăn mãi không chán bất kể trời nóng hay lạnh cho người thân của bạn. Bạn có thể ăn kèm món canh này cùng với món tôm chiên giòn cho bữa ăn thêm tròn vị.', '', 0);
-INSERT INTO `web_food`.`food` (`FoodName`,`Picture`,`Price`,`Description`, `Instruct`, `Sale`) VALUES ('Bò nướng lá lốt',NULL,270000,'Bò nướng lá lốt hay còn gọi là bò lá lốt hoặc là thịt bò lá lốt hay bò cuốn lá lốt là một món ăn trong ẩm thực Việt Nam, thịnh hành ở vùng Nam bộ, nguyên liệu chính là thịt bò và lá lốt được chế biến theo phương pháp nướng, có thể kèm theo mỡ chài. Các loại rau sống ăn kèm rất phong phú như: xà lách, húng quế, diếp cá, chuối chát, dưa leo, khế và chấm mắm nêm. Món này đặc trưng bởi vị hấp dẫn với vị ngon của thịt bò nướng lá lốt, béo của đậu phộng hòa chung vị chát của chuối, chua của khế, vị thanh thanh của nhiều loại rau giòn mát, cùng hương mắm nêm cay cay', '', 0);
 
 DROP TABLE IF EXISTS Includes; 
 create table Includes(
@@ -156,40 +165,54 @@ INSERT INTO `web_food`.`combo` (`ComboID`,`ComboName`,`ComboDescrip`,`Price`) VA
 
 
 
-drop table if exists Material;
-create table Material(
-    MaterialID bigint(8) NOT NULL AUTO_INCREMENT,
-    MaterialName varchar(255),
-    Picture text,
-    Primary key (MaterialID)
-);
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('1', 'Hành');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('2', 'Tỏi');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('3', 'Rượu');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('4', 'Giấm');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('5', 'Thịt heo');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('6', 'Trứng');
-INSERT INTO `web_food`.`material` (`MaterialID`, `MaterialName`) VALUES ('7', 'Cà chua');
+--
+-- Table structure for table `material`
+--
+
+DROP TABLE IF EXISTS `material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `material` (
+  `MaterialID` bigint NOT NULL AUTO_INCREMENT,
+  `MaterialName` varchar(255) DEFAULT NULL,
+  `Picture` text,
+  PRIMARY KEY (`MaterialID`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `material`
+--
+
+LOCK TABLES `material` WRITE;
+/*!40000 ALTER TABLE `material` DISABLE KEYS */;
+INSERT INTO `material` VALUES (10,'Trứng','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FEgg.png?alt=media&token=afe1561d-fb54-47f1-9b35-10deeaa06080'),(11,'Thịt heo ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FHam.png?alt=media&token=4004a1a1-a5f3-4f70-bbd2-b05ce2d47e37'),(12,'Cải xanh ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FLettuce.png?alt=media&token=e6bc5dd7-3b3f-4e58-86a9-429c41e3c91b'),(13,'Tỏi ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FGarlic.png?alt=media&token=47a7ec01-bff7-42a1-8690-0654d40df85b'),(14,'Ớt bầm','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FRed%20Chilli.png?alt=media&token=2aed6787-4dd7-4fa0-aa71-69f6373c57ab'),(15,'Cà chua ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FTomato.png?alt=media&token=5f6b7ace-6316-4535-bf38-ad7bb79221a3'),(16,'Chanh ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2Flemons.png?alt=media&token=a8b7ad38-66cf-4713-8671-b5113c4fe159'),(17,'Ớt xanh  ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FGreen%20Chilli.png?alt=media&token=4c669a1a-1102-425f-9a28-726f4498806f'),(18,'Dầu ô-liu  ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FOlive%20Oil.png?alt=media&token=b69add91-5a69-4e60-a8ab-3706ea10a1e3'),(19,'Thịt dê ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FGoat%20Meat.png?alt=media&token=2be6d76e-eaed-43b7-810d-58d5ab53ac93'),(20,'Sửa chua ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FGreek%20yogurt.png?alt=media&token=d15108d4-c708-4777-88ab-68e3827e9cd0'),(23,'Cà tím  ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2FAubergine.png?alt=media&token=27db63f7-9667-4c86-890c-85481277411c'),(24,'Hành đỏ ','https://firebasestorage.googleapis.com/v0/b/bk-food-sale.appspot.com/o/images%2Fzxcvzxcvzxcv%2Fred%20onions.png?alt=media&token=6ac4876f-ec5d-4a91-afa2-16f40751ad40');
+/*!40000 ALTER TABLE `material` ENABLE KEYS */;
+UNLOCK TABLES;
 
 
 
-Drop table if exists Makeby;
-create table Makeby(
-    MaterialID bigint(8) NOT NULL AUTO_INCREMENT,
-    FoodID bigint(8),
-    primary key (MaterialID,FoodID)
-);
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('5', '1');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('2', '1');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('1', '1');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('2', '2');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('6', '2');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('7', '3');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('3', '3');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('1', '4');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('5', '4');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('2', '5');
-INSERT INTO `web_food`.`makeby` (`MaterialID`, `FoodID`) VALUES ('5', '5');
+
+DROP TABLE IF EXISTS `makeby`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `makeby` (
+  `MaterialID` bigint NOT NULL AUTO_INCREMENT,
+  `FoodID` bigint NOT NULL,
+  PRIMARY KEY (`MaterialID`,`FoodID`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `makeby`
+--
+
+LOCK TABLES `makeby` WRITE;
+/*!40000 ALTER TABLE `makeby` DISABLE KEYS */;
+INSERT INTO `makeby` VALUES (1,1),(1,4),(2,1),(2,2),(2,5),(5,1),(5,4),(5,5),(6,2),(10,3),(14,3),(15,3),(24,3);
+/*!40000 ALTER TABLE `makeby` ENABLE KEYS */;
+UNLOCK TABLES;
+
 
 DROP TABLE IF EXISTS service; 
 CREATE TABLE SERVICE (
